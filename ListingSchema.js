343306536e2d8f6fd56a5a7d0c7aad3da9d2f1eb
Ori Leibovici <orileibovici@Oris-MacBook-Pro.local>
2019-09-18 16:48:20 -0400

    adding files

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,27 +1,33 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
-    Schema = mongoose.Schema;
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
 
-/* Create your schema for the data in the listings.json file that will define how data is saved in your database
-     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
-     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-  */
+// Create your schema for the data in the listings.json file that will define how data is saved in your database
 var listingSchema = new Schema({
-  /* Your code for a schema here */ 
-  //Check out - https://mongoosejs.com/docs/guide.html
-
+  code: {type: String, required: true},
+  name: { type: String, required: true},
+  coordinates: { 
+    latitude: Number,
+    longitude: Number
+  }, 
+  address: String
 });
 
-/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
-   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-*/
+// Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  // get the current date
+  var currentDate = new Date();
+
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at) this.created_at = currentDate;
+
+  next();
 });
 
-/* Use your schema to instantiate a Mongoose model */
-//Check out - https://mongoosejs.com/docs/guide.html#models
+//Use your schema to instantiate a Mongoose model
 var Listing = mongoose.model('Listing', listingSchema);
 
-/* Export the model to make it avaiable to other parts of your Node application */
+// Export the model to make it avaiable to other parts of your Node application 
 module.exports = Listing;
--- queries.js
+++ queries.js
@@ -1,39 +1,55 @@
 /* Add all the required libraries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
 
-/* Connect to your database using mongoose - remember to keep your key secret*/
+// remove limit to array elements printed to console
+const util = require('util');
+util.inspect.defaultOptions.maxArrayLength = null; 
 
-/* Fill out these functions using Mongoose queries*/
-//Check out - https://mongoosejs.com/docs/queries.html
+// Connect to your database using mongoose - remember to keep your key secret
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
 
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+  //Find the document that contains data corresponding to Library West, then log it to the console. 
+  Listing.find({ code : 'LBW' }, function (err, data) 
+  {
+    if (err) throw err;
+    console.log(data);
+  });
 };
-var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
-   */
-};
-var updatePhelpsLab = function() {
-  /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
-    Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
-   */
+var removeCable = function() {
+  // Find the document with the code 'CABL' and emove this listing from your database and log the document to the console. 
+  Listing.findOneAndRemove({ code : 'CABL' },  function (err, data) 
+  {
+    if (err) throw err;
+    console.log(data);
+  })
 };
+
+var updatePhelpsLab = function() {
+   // Phelps Lab address is incorrect. Find Phelps Lab, update it to Correct Address: 1953 Museum Rd, Gainesville, FL 32603, and then 
+   // log the updated document to the console. 
+    Listing.findOneAndUpdate({code: 'PHL'}, {address: '1953 Museum Rd, Gainesville, FL 32603' }, function(err, data)
+    {
+      if (err) throw err;
+      console.log(data)
+    })
+};
+
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+  //Retrieve all listings in the database, and log them to the console. 
+  Listing.find( function(err, data)
+  {
+    if (err) throw err;
+    console.log(data);
+  })
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
